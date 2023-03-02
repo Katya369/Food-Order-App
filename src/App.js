922053799eb70e3cc54690234c21dadcf9982cff
Ekaterina <nekrev@gmail.com>
2023-03-02 11:39:25 +0300

    Added pop-up Module

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, {Fragment} from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart.js';
+
 function App() {
   return (
    <Fragment>
+   <Cart />
     <Header />
     <main>
 <Meals />
